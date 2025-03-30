a184b065962a42a37aa9d217e871a92f9581411e
kevin <guangjinhai@163.com>
2025-03-30 09:22:37 +0800

    add scripts

---
+++ notebooks/download.bash
@@ -0,0 +1,2 @@
+curl https://zh-v2.d2l.ai/d2l-zh-2.0.0.zip -o d2l-zh.zip
+unzip d2l-zh.zip && rm d2l-zh.zip
---
+++ notebooks/pytorch/install.bash
@@ -0,0 +1,6 @@
+
+# conda activate d2l
+pip install torch==1.12.0
+pip install torchvision==0.13.0
+
+pip install ipykernel pyprocess
